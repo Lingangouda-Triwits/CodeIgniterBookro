f651463b81e398880667ad7f224ed97b3a6cb473
OnkarTriwits <114130214+OnkarTriwits@users.noreply.github.com>
2023-06-19 16:50:45 +0530

    okk

--- application/views/admin/LoginView.php
+++ application/views/admin/LoginView.php
@@ -63,7 +63,7 @@
         <div class="input-group mb-3">
           <input type="password" name="password" id="password" class="form-control" placeholder="Password" style="background-color: #e3f2fd;">
           <div class="input-group-append">
-            <div class="input-group-text">
+            <div class="input-group-text"> 
               <span class="fas fa-lock"></span>
             </div>
           </div>
